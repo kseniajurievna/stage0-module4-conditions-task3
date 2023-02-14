3dee7ab93bb9b91a0f3c07fc7480f5d5a6d45e58
kseniakaratchenia <62858256+kseniakaratchenia@users.noreply.github.com>
2023-02-13 16:29:29 -0800

    fix2

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -1,13 +1,11 @@
 package school.mjc.stage0.conditions.task3;
-import java.util.logging.Logger;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
-        Logger logger = Logger.getGlobal();
         if(first % second == 0){
-            logger.info("Aliquot");
+            System.out.println("Aliquot");
         } else {
-            logger.info("Not aliquot");
+            System.out.println("Not aliquot");
         }
     }
 }
